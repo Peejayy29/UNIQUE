7ecb082aad900382755f95cd80ea1b31cda0bb22
Aryan M. P <101510330+Aryan-MP@users.noreply.github.com>
2024-02-15 17:17:12 +0530

    structred the folders properly

--- script.js
+++ script.js
@@ -6,19 +6,4 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('open');
         navMenu.classList.toggle('open');
     });
-
-    // Function to handle carousel effect
-    function carouselSlide() {
-        const carousel = document.querySelector('.carousel');
-        carousel.style.transition = 'transform 0.5s ease-in-out'; // Add transition effect
-        const firstProduct = carousel.children[0];
-        carousel.style.transform = 'translateX(-100%)'; // Move carousel to the left
-        setTimeout(function() {
-            carousel.appendChild(firstProduct); // Move first product to the end of the carousel
-            carousel.style.transition = 'none'; // Remove transition to instantly move to the new position
-            carousel.style.transform = 'translateX(0)'; // Reset carousel position
-        }, 500); // Wait for transition to complete before resetting position
-    }
-
-    setInterval(carouselSlide, 3000); // Change slide every 3 seconds
 });
